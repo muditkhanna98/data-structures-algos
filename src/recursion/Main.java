673565c258b90d487b2cd24c6d1aaf1d8aa3998e
Mudit Khanna <muditkhanna998@gmail.com>
2023-09-23 17:48:03 -0400

    adds jos problem

--- src/recursion/Main.java
+++ src/recursion/Main.java
@@ -2,7 +2,7 @@ package recursion;
 
 public class Main {
     public static void main(String[] args) {
-        print1ToN(5);
+        System.out.println(jos(7, 3));
     }
 
     private static void printNTo1(int digit) {
@@ -16,4 +16,41 @@ public class Main {
         print1ToN(digit - 1);
         System.out.println(digit);
     }
+
+    private static int factorial(int digit) {
+        if (digit == 0) return 1;
+        return digit * factorial(digit - 1);
+    }
+
+    private static int fibonacci(int num) {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+
+    private static int sumOfNNumbers(int digit) {
+        if (digit == 0) return 0;
+        return digit + sumOfNNumbers(digit - 1);
+    }
+
+    private static int sumOfDigits(int digit) {
+        if (digit == 0) return 0;
+        return digit % 10 + sumOfDigits(digit / 10);
+    }
+
+    private static int maxPieces(int length, int a, int b, int c) {
+        if (length == 0) return 0;
+        if (length < 0) return -1;
+
+        int result = Math.max(Math.max(maxPieces(length - a, a, b, c), maxPieces(length - b, a, b, c)), maxPieces(length - c, a, b, c));
+        if (result == -1) return -1;
+        return result + 1;
+    }
+
+    //josephus problem (Microsoft question)
+    private static int jos(int totalNumberOfPeople, int personToBeKilled) {
+        if (totalNumberOfPeople == 1) return 0;
+        return (jos(totalNumberOfPeople - 1, personToBeKilled) + personToBeKilled) % totalNumberOfPeople;
+    }
+
 }
