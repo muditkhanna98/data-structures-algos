2e99321f286f9e5abbb11d6292645fa55b23b6d4
Mudit Khanna <muditkhanna998@gmail.com>
2023-09-26 17:54:12 -0400

    implementation of tree

---
+++ src/trees/Tree.java
@@ -0,0 +1,60 @@
+package trees;
+
+public class Tree {
+    private class Node {
+        private int value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    '}';
+        }
+    }
+
+    private Node root;
+
+    public void insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        Node current = root;
+        while (true) {
+            if (value < current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = newNode;
+                    break;
+                }
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = newNode;
+                    break;
+                }
+                current = current.rightChild;
+            }
+        }
+    }
+
+    public boolean find(int value) {
+        Node current = root;
+        while (current != null) {
+            if (value < current.value) {
+                current = current.leftChild;
+            } else if (value > current.value) {
+                current = current.rightChild;
+            } else return true;
+        }
+
+        return false;
+    }
+}
