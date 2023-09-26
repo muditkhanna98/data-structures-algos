567c167261526720fa72c8fdc4ef52f0137a486f
Mudit Khanna <muditkhanna998@gmail.com>
2023-09-26 08:19:42 -0400

    adds merge sort

--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] array = {3, 2, 10, 3, 9};
-        selectionSort(array);
+        mergeSort(array);
         System.out.println(Arrays.toString(array));
     }
 
@@ -39,6 +39,61 @@ public class Main {
             int temp = array[i];
             array[i] = array[minimumIndex];
             array[minimumIndex] = temp;
+        }
+    }
+
+    //O(n2)
+    private static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > current) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = current;
+        }
+    }
+
+    //O(n log n)
+    private static void mergeSort(int[] array) {
+        // Divide this array into half
+        if (array.length < 2) return;
+
+        int middle = array.length / 2;
+        int[] left = new int[middle];
+
+        for (int i = 0; i < middle; i++) {
+            left[i] = array[i];
+        }
+
+        int[] right = new int[array.length - middle];
+        for (int i = middle; i < array.length; i++) {
+            right[i - middle] = array[i];
+        }
+
+        // Sort each half
+        mergeSort(left);
+        mergeSort(right);
+        merge(array, left, right);
+    }
+
+    private static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) array[k++] = left[i++];
+            else array[k++] = right[j++];
+        }
+
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            array[k++] = right[j++];
         }
     }
 
