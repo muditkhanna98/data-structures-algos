593060702322e1dbc9b6e8be664f6fddc98eab8e
Mudit Khanna <muditkhanna998@gmail.com>
2023-10-07 19:08:43 -0400

    adds stock buy sell problem

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -1,11 +1,13 @@
 package arrays;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 0, 5, 6, 8, 7};
-        System.out.println(getSecondLargest(arr));
+        int[] arr = {0, 10, 5, 6, 10, 7, 6};
+        System.out.println(maxProfit(arr));
     }
 
     private static int largest(int[] arr) {
@@ -38,17 +40,16 @@ public class Main {
         return secondLargest;
     }
 
-    public static boolean ifSorted(int[] arr) {
-        for (int i = 0; i <= arr.length - 2; i++) {
+    private static boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i + 1] < arr[i]) return false;
         }
 
         return true;
     }
 
-    public static void reverse(int[] arr) {
-        int low = 0;
-        int high = arr.length - 1;
+    private static void reverse(int[] arr) {
+        int low = 0, high = arr.length - 1;
 
         while (low < high) {
             int temp = arr[low];
@@ -61,22 +62,63 @@ public class Main {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void moveZeroToEnd(int[] arr) {
-        int count = 0;
-        for (int i = 0; i <= arr.length - 1; i++) {
-            if (arr[i] != 0) {
-                arr[count] = arr[i];
-                count++;
-            }
+    private static void leftRotateBy1(int[] arr) {
+        int temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
         }
 
-        while (count <= arr.length - 1) {
-            arr[count] = 0;
-            count++;
-        }
-
+        arr[arr.length - 1] = temp;
 
         System.out.println(Arrays.toString(arr));
     }
 
+    private static void leftRotateByD(int[] arr, int d) {
+        // do above function inside a loop
+    }
+
+    private static void printLeaders(int[] arr) {
+        //all the elements that are greater than all the elements on its right are leaders
+        //right most element is always a leader
+        int current = arr[arr.length - 1];
+        System.out.println(current);
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > current) {
+                current = arr[i];
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+
+    private static void frequencies(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> value : map.entrySet()) {
+            System.out.println(value.getKey() + " " + value.getValue());
+        }
+    }
+
+    //stock buy and sell problem
+    private static int maxProfit(int[] arr) {
+        int maxProfit = 0;
+        int minSoFar = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            minSoFar = Math.min(minSoFar, arr[i]);
+            int profit = arr[i] - minSoFar;
+            maxProfit = Math.max(profit, maxProfit);
+        }
+
+        return maxProfit;
+    }
 }
