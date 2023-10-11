4bb1062a2144e7e88faba90dbaa2fdf42a262125
Mudit Khanna <muditkhanna998@gmail.com>
2023-10-11 15:37:07 -0400

    trapping rain water

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -6,8 +6,8 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 5, 3, 8, 12};
-        System.out.println(maxProfitEfficient(arr));
+        int[] arr = {3, 0, 1, 2, 5};
+        System.out.println(trappingRainWaterEfficient(arr));
     }
 
     private static int largest(int[] arr) {
@@ -142,6 +142,48 @@ public class Main {
         for (int i = 1; i < arr.length; i++) {
             result = Math.max(result, arr[i] - minimum);
             minimum = Math.min(minimum, arr[i]);
+        }
+
+        return result;
+    }
+
+    private static int trappingRainWater(int[] arr) {
+        int result = 0;
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            int lMax = arr[i];
+            for (int j = 0; j < i; j++) {
+                lMax = Math.max(lMax, arr[j]);
+            }
+
+            int rMax = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                rMax = Math.max(rMax, arr[j]);
+            }
+
+            result = result + Math.min(lMax, rMax) - arr[i];
+        }
+
+        return result;
+    }
+
+    private static int trappingRainWaterEfficient(int[] arr) {
+        int result = 0;
+        int[] lMax = new int[arr.length];
+        int[] rMax = new int[arr.length];
+
+        lMax[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            lMax[i] = Math.max(arr[i], lMax[i - 1]);
+        }
+
+        rMax[arr.length - 1] = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            rMax[i] = Math.max(arr[i], rMax[i + 1]);
+        }
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            result = result + Math.min(lMax[i], rMax[i]) - arr[i];
         }
 
         return result;
