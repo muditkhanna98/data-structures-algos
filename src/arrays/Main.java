9310c48bf38ecfe9acd60260712060bd3d3854fe
Mudit Khanna <muditkhanna998@gmail.com>
2023-11-10 20:18:41 -0500

    adds maximum sum subarray problem

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 0, 4, 5, 3, 0, 6, 7, 8};
+        int[] arr = {7, 10, 4, 3, 20, 5, 2};
 
-        moveZeroesToEnd(arr);
+        System.out.println(maximumDifference(arr));
     }
 
     private static int largestElement(int[] arr) {
@@ -79,7 +79,99 @@ public class Main {
 
     }
 
+    private static int maxSumSubArray(int[] arr, int k) {
+        int wSum = 0;
+        int maxSum = Integer.MIN_VALUE;
 
+        for (int i = 0; i < k; i++) {
+            wSum += arr[i];
+        }
+
+        for (int i = k; i < arr.length; i++) {
+            wSum = wSum - arr[i - k] + arr[i];
+            maxSum = Math.max(wSum, maxSum);
+        }
+
+        return maxSum;
+    }
+
+    private static boolean subArrayWithGivenSum(int[] arr, int sum) {
+        int start = 0;
+        int cSum = 0;
+
+        for (int end = 0; end < arr.length; end++) {
+            cSum += arr[end];
+
+            while (sum < cSum) {
+                cSum -= arr[start];
+                start++;
+            }
+
+            if (cSum == sum) return true;
+        }
+
+        return false;
+    }
+
+    private static void leftRotateArrayByOne(int[] arr) {
+        int firstElement = arr[0];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[arr.length - 1] = firstElement;
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void leftRotateArrayByD(int[] arr, int d) {
+        //first reverse d elements
+        //second reverse the n-d elements
+        //third reverse the whole array
+        if (arr.length == 1) return;
+        d = d % arr.length;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, arr.length - 1);
+        reverse(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void reverse(int[] arr, int low, int high) {
+        while (low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+
+    private static void printLeaders(int[] arr) {
+        int currentLeader = arr[arr.length - 1];
+        System.out.println(currentLeader);
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > currentLeader) {
+                currentLeader = arr[i];
+                System.out.println(currentLeader);
+            }
+        }
+    }
+
+    private static int maximumDifference(int[] arr) {
+        int min = arr[0];
+        int maxDifference = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            maxDifference = Math.max(maxDifference, arr[i] - min);
+            min = Math.min(min, arr[i]);
+        }
+
+        return (maxDifference > 0) ? maxDifference : -1;
+    }
 
 
 }
